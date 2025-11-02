fb1813c43847fd55591c02e97dc5ad91515011b2
Benny <ben@bencode.net>
2025-11-02 23:59:20 +0000

    forced merge stewardships samples

--- SzConnyApp/SenzingV4/Commands/ForceResolveCommand.cs
+++ SzConnyApp/SenzingV4/Commands/ForceResolveCommand.cs
@@ -15,45 +15,45 @@ public class ForceResolveCommand(
     {
         // Stage 0: Purge senzing repository `./SzConnyApp purge`
         
-        
-        // Stage 1: Seed data
         var sampleRecords = GetSampleRecords();
         
-        foreach (var nestedRecord in sampleRecords)
-        {
-            var addRecordResponse = szEnvironment.AddNestedRecord(nestedRecord);
-
-            logger.LogInformation(
-                $"Record {addRecordResponse?.RecordId} added, affected entities: {addRecordResponse?.AffectedEntities.Count}"
-            );
-
-            if ((bool)addRecordResponse?.AffectedEntities.Any())
-            {
-                foreach (var affectedEntity in addRecordResponse?.AffectedEntities)
-                {
-                    logger.LogInformation($"Affected Entity ID: {affectedEntity?.EntityId}");
-                }
-            }
-        }
+        
+        // Stage 1: Seed data `./SzConnyApp force`
+        // foreach (var nestedRecord in sampleRecords)
+        // {
+        //     var addRecordResponse = szEnvironment.AddNestedRecord(nestedRecord);
+        //
+        //     logger.LogInformation(
+        //         $"Record {addRecordResponse?.RecordId} added, affected entities: {addRecordResponse?.AffectedEntities.Count}"
+        //     );
+        //
+        //     if ((bool)addRecordResponse?.AffectedEntities.Any())
+        //     {
+        //         foreach (var affectedEntity in addRecordResponse?.AffectedEntities)
+        //         {
+        //             logger.LogInformation($"Affected Entity ID: {affectedEntity?.EntityId}");
+        //         }
+        //     }
+        // }
 
         
         // Stage 2: Run 'SzConnyApp export' or sz_explorer - there should be 3 discrete entities
 
         
-        // Stage 3: Force resolve record 101 and 103
-        // foreach (var record in new[] { sampleRecords[0], sampleRecords[2] })
-        // {
-        //     var trustedIdFeature = new SenzingEntitySpecification
-        //     {
-        //         TrustedIdNumber = "TEST_101-TEST_103",
-        //         TrustedIdType = SzTrustedIdType.ForceResolve
-        //     };
-        //     record.Features.Add(trustedIdFeature);
-        //     var addRecordResponse = szEnvironment.AddNestedRecord(record);
-        //     logger.LogInformation(
-        //         $"Record {addRecordResponse?.RecordId} added with TRUSTED_ID, affected entities: {addRecordResponse?.AffectedEntities.Count}"
-        //     );
-        // }
+        // Stage 3: Force resolve record 101 and 103, uncomment and run `./SzConnyApp force`
+        foreach (var record in new[] { sampleRecords[0], sampleRecords[2] })
+        {
+            var trustedIdFeature = new SenzingEntitySpecification
+            {
+                TrustedIdNumber = "TEST_101-TEST_103",
+                TrustedIdType = SzTrustedIdType.ForceResolve
+            };
+            record.Features.Add(trustedIdFeature);
+            var addRecordResponse = szEnvironment.AddNestedRecord(record);
+            logger.LogInformation(
+                $"Record {addRecordResponse?.RecordId} added with TRUSTED_ID, affected entities: {addRecordResponse?.AffectedEntities.Count}"
+            );
+        }
 
 
         // Stage 4: Run 'SzConnyApp export' or sz_explorer `why 1` - there should be 2 discrete entities
